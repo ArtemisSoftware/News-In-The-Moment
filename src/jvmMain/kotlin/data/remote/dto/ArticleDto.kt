542a40ba1a82060b640a3ed5bc4211fe2310cefa
gustavo <gustavo.isaac@gmail.com>
2023-10-23 04:58:59 +0100

    ArticleDto

---
+++ src/jvmMain/kotlin/data/remote/dto/ArticleDto.kt
@@ -0,0 +1,24 @@
+package data.remote.dto
+
+import com.google.gson.annotations.SerializedName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ArticleDto(
+    @SerializedName("author")
+    val author: String?,
+    @SerializedName("content")
+    val content: String?,
+    @SerializedName("description")
+    val description: String?,
+    @SerializedName("publishedAt")
+    val publishedAt: String,
+    @SerializedName("source")
+    val source: SourceDto,
+    @SerializedName("title")
+    val title: String?,
+    @SerializedName("url")
+    val url: String?,
+    @SerializedName("urlToImage")
+    val urlToImage: String?
+)
