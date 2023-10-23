12b2a86a8ea0165a82fbf9595887a78a02ab8be6
gustavo <gustavo.isaac@gmail.com>
2023-10-23 04:59:36 +0100

    NewsDto

---
+++ src/jvmMain/kotlin/data/remote/dto/NewsDto.kt
@@ -0,0 +1,14 @@
+package data.remote.dto
+
+import com.google.gson.annotations.SerializedName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class NewsDto(
+    @SerializedName("articles")
+    val articles: List<ArticleDto>,
+    @SerializedName("status")
+    val status: String,
+    @SerializedName("totalResults")
+    val totalResults: Int
+)
