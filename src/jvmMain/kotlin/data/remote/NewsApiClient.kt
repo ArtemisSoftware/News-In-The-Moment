bd611fe893ecf0c25f15d4633965be8fc3c646b6
gustavo <gustavo.isaac@gmail.com>
2023-10-23 05:12:46 +0100

    NewsApiClient

---
+++ src/jvmMain/kotlin/data/remote/NewsApiClient.kt
@@ -0,0 +1,29 @@
+package data.remote
+
+import data.remote.dto.NewsDto
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.*
+import io.ktor.serialization.kotlinx.json.*
+import util.Constants
+
+object NewsApiClient {
+
+    private val client = HttpClient(CIO) {
+        install(ContentNegotiation) {
+            json()
+        }
+    }
+
+    suspend fun getTopHeadlines(): NewsDto {
+        val url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=${Constants.API_KEY}"
+        return client.get(url).body()
+    }
+
+    suspend fun getSearchedNews(searchedText: String): NewsDto {
+        val url = "https://newsapi.org/v2/everything?q=$searchedText&apiKey=${Constants.API_KEY}"
+        return client.get(url).body()
+    }
+}
