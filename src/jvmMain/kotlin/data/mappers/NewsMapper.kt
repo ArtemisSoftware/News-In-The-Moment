c76b454a323a8992aa2c1e40282ed4194aaca177
gustavo <gustavo.isaac@gmail.com>
2023-10-24 04:45:16 +0100

    NewsMapper

---
+++ src/jvmMain/kotlin/data/mappers/NewsMapper.kt
@@ -0,0 +1,13 @@
+package data.mappers
+
+import data.remote.dto.ArticleDto
+import domain.models.Article
+
+fun ArticleDto.toArticle(): Article{
+    return Article(
+        url = url,
+        imageUrl = urlToImage,
+        title = title ?: "",
+        content = content ?: "",
+    )
+}
--- src/jvmMain/kotlin/domain/models/Article.kt
+++ src/jvmMain/kotlin/domain/models/Article.kt
@@ -1,8 +1,8 @@
 package domain.models
 
 data class Article(
-    val url: String,
-    val imageUrl: String,
+    val url: String? = null,
+    val imageUrl: String? = null,
     val title: String,
     val content: String,
 )
