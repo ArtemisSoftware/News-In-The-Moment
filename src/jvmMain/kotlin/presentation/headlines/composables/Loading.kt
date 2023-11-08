e411f89fcc4abbb738e98eeae076e2e9a58862ee
gustavo <gustavo.isaac@gmail.com>
2023-11-08 05:52:39 +0000

    Loading

---
+++ src/jvmMain/kotlin/presentation/headlines/composables/Loading.kt
@@ -0,0 +1,44 @@
+package presentation.headlines.composables
+
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.rotate
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun Loading() {
+    Column(
+        modifier = Modifier.fillMaxSize().background(Color.White),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        content = {
+            val infiniteTransition = rememberInfiniteTransition()
+            val rotation by infiniteTransition.animateFloat(
+                initialValue = 0f,
+                targetValue = 360f,
+                animationSpec = infiniteRepeatable(
+                    animation = tween<Float>(
+                        durationMillis = 3000,
+                        easing = FastOutLinearInEasing,
+                    ),
+                ),
+            )
+
+            Canvas(modifier = Modifier.size(250.dp)) {
+                rotate(rotation) {
+                    drawRect(color = Color(50, 205, 50))
+                }
+            }
+        },
+    )
+}
