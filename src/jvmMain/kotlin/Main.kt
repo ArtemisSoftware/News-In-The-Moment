ce60039a48cde132b67a910e6d9ef0380338e079
gustavo <gustavo.isaac@gmail.com>
2023-11-08 05:58:32 +0000

    App title

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,16 +1,13 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.material.MaterialTheme
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
-import data.repository.NewsRepositoryImpl
 import di.appModule
-import domain.repository.NewsRepository
 import org.koin.core.context.GlobalContext.startKoin
-import org.koin.java.KoinJavaComponent
 import presentation.headlines.HeadlineScreen
 
 @Composable
@@ -27,8 +24,9 @@ fun main() = application {
     }
 
     Window(
+        title = "News In The Moment",
         onCloseRequest = ::exitApplication,
-        rememberWindowState(size = DpSize(1200.dp, 1200.dp)),
+        state = rememberWindowState(size = DpSize(1200.dp, 1200.dp)),
         resizable = false,
     ) {
         App()
